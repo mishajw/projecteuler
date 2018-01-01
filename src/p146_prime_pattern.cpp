3ff4a8040d1a0b05c88a93c3a4b120b6968020e0
Misha Wagner <mishajw@gmail.com>
2018-01-01 15:48:45 +0000

    Add complete solution to 146

--- src/p146_prime_pattern.cpp
+++ src/p146_prime_pattern.cpp
@@ -1,27 +1,46 @@
-// Find all numbers `n` for which...
-// `n^2 + 1`, `n^2 + 3`, `n^2 + 7`, `n^2 + 9`, `n^2 + 13`, `n^2 + 27`
-// ...are primes, where `n` lower than 150,000,000. Return the sum of this set.
-
-#include <inttypes.h>
 #include <iostream>
+#include <inttypes.h>
+#include <vector>
+#include <cmath>
 
-typedef uint32_t int_t;
+typedef uint64_t int_t;
 typedef uint64_t bigint_t;
+
+static const int_t LIMIT = 150000000;
 static const int_t INCRS[] = {1, 3, 7, 9, 13, 27};
+static const int_t NON_PRIME_INCRS[] = {5, 11, 15, 17, 19, 21, 23, 25};
+
+bool check_are_primes(const std::vector<bigint_t> &xs, const int_t &limit) {
+  for (const auto &x : xs) {
+    if (!(x % 2) || !(x % 3)) {
+      return false;
+    }
+  }
+
+  int_t i = 5;
+  int_t w = 2;
+  while (i <= limit) {
+    for (const auto &x : xs) {
+      if (i * i <= x && x % i == 0) {
+        return false;
+      }
+    }
+
+    i += w;
+    w = 6 - w;
+  }
+
+  return true;
+}
 
 bool is_prime(const bigint_t &x, const int_t &limit) {
-  // Not needed, as we never test 2 or 3 for being prime
-  // if (x == 2 || x == 3) {
-  //   return true;
-  // }
-
   if (!(x % 2) || !(x % 3)) {
     return false;
   }
 
   int_t i = 5;
   int_t w = 2;
-  while (i < limit) {
+  while (i <= limit) {
     if (x % i == 0) {
       return false;
     }
@@ -38,25 +57,32 @@ bool check_number(const int_t &x) {
   }
 
   bigint_t squared = bigint_t(x) * bigint_t(x);
-  for (const auto &i : INCRS) {
-    if (!is_prime(squared + i, x + 17)) {
-      return false;
-    }
-  }
+  bigint_t limit = std::sqrt(squared + 27);
 
-  return true;
+  std::vector<bigint_t> possible_primes;
+  for (const auto &i : INCRS) {
+    possible_primes.push_back(squared + i);
+  }
+  bool are_primes = check_are_primes(possible_primes, limit);
+  
+  if (are_primes) {
+    for (const auto &i : NON_PRIME_INCRS) {
+      if (is_prime(squared + i, limit)) {
+        return false;
+      }
+    }
+
+    return true;
+  } else {
+    return false;
+  }
 }
 
 int main() {
   int_t total = 0;
-  for (int_t x = 10; x <= 300000; x += 2) {
+  for (int_t x = 10; x <= LIMIT; x += 2) {
     if (check_number(x)) {
       total += x;
-      std::cout << "Found match: " << x << std::endl;
-    }
-
-    if (x % 10000 == 0) {
-      std::cout << "Checking " << x << std::endl;
     }
   }
 
